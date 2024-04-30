07b5e0849122e4c28c10521a0e72d93b6c677ca9
Shreyas M S <shreyasmadappady20@gmail.com>
2024-04-30 21:36:44 +0530

    c

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,11 @@ import NavBar from "./components/NavBar";
 import Places from "./components/Places";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [link, setLink] = useState(null);
-
   return (
     <div>
       <NavBar />
-      <Places setLink={setLink} setIsOpen={setIsOpen} />
+      <Places />
       {/* <NearbyPlace waypoint={{lat:12.5581, lng:75.3908 }} radius={10000}/> */}
-      {isOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <iframe src={props.link} title="External Webpage" />
-            <button onClick={handleClose}>Close</button>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
--- client/src/components/DisplayPlaces.jsx
+++ client/src/components/DisplayPlaces.jsx
@@ -2,95 +2,108 @@ import StarRatings from "react-star-ratings";
 import ImageRender from "./ImageRender";
 import Scroll from "react-scroll";
 import svg from "/src/images/external.svg";
+import { useState } from "react";
 
 function DisplayPlaces({ nearbyPlaces, SetDistinctMarker }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   const ScrollLink = Scroll.Link;
   return (
-    <div className="mx-4 my-8">
-      <h2 className="pb-2 pl-8 text-xl text-slate-800">
-        Results ({nearbyPlaces.length})
-      </h2>
+    <>
+      <div className="mx-4 my-8">
+        <h2 className="pb-2 pl-8 text-xl text-slate-800">
+          Results ({nearbyPlaces.length})
+        </h2>
 
-      <ul className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-        {nearbyPlaces.map((place, index) => (
-          <div
-            key={index}
-            className="flex flex-row items-start justify-between pb-4 border-solid border-1black border-[1px] px-8 py-4"
-          >
-            <div className="flex flex-col justify-center">
-              <li className="text-xl font-medium">{place.place}</li>
-              {place.data.rating ? (
-                <div className="flex items-center justify-start gap-1">
-                  <h1 className="text-sm text-slate-500">
-                    {place.data.rating || "0.0"}
-                  </h1>
-                  <div className="pb-0.5">
-                    <StarRatings
-                      rating={place.data.rating}
-                      starRatedColor="#fbbc04"
-                      // changeRating={this.changeRating}
-                      numberOfStars={5}
-                      name="rating"
-                      starDimension="15px"
-                      starSpacing="1px"
-                    />
+        <ul className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+          {nearbyPlaces.map((place, index) => (
+            <div
+              key={index}
+              className="flex flex-row items-start justify-between pb-4 border-solid border-1black border-[1px] px-8 py-4"
+            >
+              <div className="flex flex-col justify-center">
+                <li className="text-xl font-medium">{place.place}</li>
+                {place.data.rating ? (
+                  <div className="flex items-center justify-start gap-1">
+                    <h1 className="text-sm text-slate-500">
+                      {place.data.rating || "0.0"}
+                    </h1>
+                    <div className="pb-0.5">
+                      <StarRatings
+                        rating={place.data.rating}
+                        starRatedColor="#fbbc04"
+                        // changeRating={this.changeRating}
+                        numberOfStars={5}
+                        name="rating"
+                        starDimension="15px"
+                        starSpacing="1px"
+                      />
+                    </div>
+                    <span className="text-sm text-slate-500">
+                      ({place.data.user_ratings_total || 0})
+                    </span>{" "}
                   </div>
-                  <span className="text-sm text-slate-500">
-                    ({place.data.user_ratings_total || 0})
-                  </span>{" "}
-                </div>
-              ) : (
-                <div className="flex items-center justify-start gap-1">
-                  <h1 className="text-sm text-slate-500">No reviews</h1>
-                </div>
-              )}
+                ) : (
+                  <div className="flex items-center justify-start gap-1">
+                    <h1 className="text-sm text-slate-500">No reviews</h1>
+                  </div>
+                )}
 
-              <h1 className="text-sm text-slate-500">{place.data.vicinity}</h1>
-              <h2 className="text-sm text-slate-500">
-                {place.data.business_status === "OPERATIONAL" && (
-                  <span className="text-green-600">Open</span>
-                )}
-                {place.data.business_status === "CLOSED_TEMPORARILY" && (
-                  <span className="text-[#dd3d3d]">Temporarily closed</span>
-                )}
-              </h2>
-              <div className="flex gap-4">
-                {" "}
-                <ScrollLink
-                  to="footer"
-                  offset={-50}
-                  smooth={true}
-                  duration={500}
-                >
-                  <button
-                    className=" rounded-md bg-[#1a73e8] text-white px-2 py-1 text-sm"
-                    onClick={() => {
-                      console.log(place.data.geometry.location.lat());
-                      console.log(place.data.place_id);
-                      SetDistinctMarker({
-                        lat: place.data.geometry.location.lat(),
-                        lng: place.data.geometry.location.lng(),
-                        place: place.place,
-                      });
-                    }}
+                <h1 className="text-sm text-slate-500">
+                  {place.data.vicinity}
+                </h1>
+                <h2 className="text-sm text-slate-500">
+                  {place.data.business_status === "OPERATIONAL" && (
+                    <span className="text-green-600">Open</span>
+                  )}
+                  {place.data.business_status === "CLOSED_TEMPORARILY" && (
+                    <span className="text-[#dd3d3d]">Temporarily closed</span>
+                  )}
+                </h2>
+                <div className="flex gap-4">
+                  {" "}
+                  <ScrollLink
+                    to="footer"
+                    offset={-50}
+                    smooth={true}
+                    duration={500}
                   >
-                    Show in above map
+                    <button
+                      className=" rounded-md bg-[#1a73e8] text-white px-2 py-1 text-sm"
+                      onClick={() => {
+                        console.log(place.data.geometry.location.lat());
+                        console.log(place.data.place_id);
+                        SetDistinctMarker({
+                          lat: place.data.geometry.location.lat(),
+                          lng: place.data.geometry.location.lng(),
+                          place: place.place,
+                        });
+                      }}
+                    >
+                      Show in above map
+                    </button>
+                  </ScrollLink>
+                  {/* <a
+                    href={`https://www.google.com/maps/place/?q=place_id:${place.data.place_id}`}
+                    target="_blank"
+                    className="flex gap-2 bg-[#1a73e8] px-2 py-1 text-sm text-white rounded-md "
+                  > */}
+                  <button
+                    onClick={() => {
+                      setIsOpen(true);
+                    }}
+                    className=""
+                  >
+                    Open in google map
                   </button>
-                </ScrollLink>
-                <a
-                  href={`https://www.google.com/maps/place/?q=place_id:${place.data.place_id}`}
-                  target="_blank"
-                  className="flex gap-2 bg-[#1a73e8] px-2 py-1 text-sm text-white rounded-md "
-                >
-                  <button className="">Open in google map</button>
                   <img className="w-4 text-white" src={svg} alt="" />
-                </a>
+                  {/* </a> */}
+                </div>
               </div>
-            </div>
 
-            <ImageRender place={place} />
+              <ImageRender place={place} />
 
-            {/* <button
+              {/* <button
           onClick={() => {
             console.log(place.photo[0].getUrl());
             let data = [];
@@ -105,10 +118,28 @@ function DisplayPlaces({ nearbyPlaces, SetDistinctMarker }) {
         >
           Remove
         </button> */}
+            </div>
+          ))}
+        </ul>
+      </div>{" "}
+      {isOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <iframe
+              src="https://www.google.com/"
+              title="External Webpage"
+            />
+            <button
+              onClick={() => {
+                setIsOpen(false);
+              }}
+            >
+              Close
+            </button>
           </div>
-        ))}
-      </ul>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
 
