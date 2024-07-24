f90f06c27d04ca8bcca0bf0039a576b7784a7baa
Shreyas M S <shreyasmadappady20@gmail.com>
2024-07-24 09:33:33 +0530

    Commit

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import svg from "../images/Navicon.svg";
 import svg2 from "../images/github.png";
 function NavBar() {
   return (
-    <div className="flex items-center justify-between px-4 py-2 mb-6 bg-white shadow-lg lg:px-10 ">
+    <div className="flex items-center justify-between px-4 py-2 bg-white shadow-lg lg:px-10 ">
       <img src={svg} className="   lg:w-[150px] w-[100px] " alt="" />
       <img src={svg2} className="h-5 lg:h-6" alt="" />
     </div>
---
+++ client/src/components/TagLineBody.jsx
@@ -0,0 +1,13 @@
+
+
+function TagLineBody() {
+  return (
+    <div className="flex justify-center mb-10 bg-slate-100">
+      <h1 className="text-sm font-medium w-[80%] text-center  text-slate-800 uppercase my-[20%] md:font-bold md:text-4xl md:w-[70%] md:my-[10%] leading-10 ">
+        AI-Driven Adventures: Find the Places Between Destinations
+      </h1>
+    </div>
+  );
+}
+
+export default TagLineBody;
